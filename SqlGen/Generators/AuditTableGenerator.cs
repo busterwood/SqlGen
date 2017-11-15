e986c9a7a7f3095c5a75664109b00d0dcc72c4b4
BusterWood <chris.j.austin@gmail.com>
2017-11-15 16:02:47 +0000

    Update audit table generator

--- SqlGen/Generators/AuditTableGenerator.cs
+++ SqlGen/Generators/AuditTableGenerator.cs
@@ -14,21 +14,22 @@ namespace SqlGen
             foreach (var col in table.Columns)
             {
                 var nullable = col.IsNullable() ? "NULL" : "NOT NULL";
-                sb.AppendLine($"  [{col.ColumnName}] {col.TypeDeclaration()} {nullable},");
+                sb.AppendLine($"    [{col.ColumnName}] {col.TypeDeclaration()} {nullable},");
             }
 
-            sb.AppendLine($"  [AUDIT_TYPE] char(1) NOT NULL,");
-            sb.AppendLine($"  [AUDIT_END_DATE] datetime NOT NULL,");
-            sb.Append($"  CONSTRAINT [PK_{table.TableName}_AUDIT] PRIMARY KEY CLUSTERED (");
+            sb.AppendLine($"    [AUDIT_TYPE] char(1) NOT NULL,");
+            sb.AppendLine($"    [AUDIT_END_DATE] datetime NOT NULL,");
+            sb.Append($"    CONSTRAINT [PK_{table.TableName}_AUDIT] PRIMARY KEY CLUSTERED (");
             foreach (var c in table.PrimaryKeyColumns)
             {
                 sb.Append($"[{c.ColumnName}], ");
             }
-            sb.AppendLine(" [SEQUENCE_NUMBER])");
+            sb.AppendLine("[SEQUENCE_NUMBER])");
             sb.AppendLine(");");
             return sb.ToString();
         }
         public override string ToString() => "Audit Table";
 
+        public override string Grant(Table table, ForeignKey fk = null) => "";
     }
 }
