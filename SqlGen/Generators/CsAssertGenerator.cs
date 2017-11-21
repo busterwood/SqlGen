a2784bfdd474eed5b927b20d294deee0041f45fd
BusterWood <chris.j.austin@gmail.com>
2017-11-21 14:25:58 +0000

    Add AssertAreEqual C# code generation

---
+++ SqlGen/Generators/CsAssertGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace SqlGen
+{
+    class CsAssertGenerator : Generator
+    {
+        public override string Generate(Table table, TableKey key, bool alter)
+        {
+            var csClassName = table.TableName.ToPascalCase();
+            var csArgsName = char.ToLower(csClassName[0]) + csClassName.Substring(1);
+            var sb = new StringBuilder();
+            sb.AppendLine($"\t\tvoid AssertAreEqual({csClassName} expected, {csClassName} actual)");
+            sb.AppendLine("\t\t{");
+            foreach (var c in table.Columns.Where(c => !c.IsRowVersion()))
+            {
+                var propName = c.ColumnName.ToPascalCase();
+                sb.AppendLine($"\t\t\tAssert.AreEqual(expected.{propName}, actual.{propName}, \"{propName}\");");
+            }
+
+            sb.AppendLine("\t\t}");
+            return sb.ToString();
+        }
+
+        public override string ToString() => "C# AssertAreEqual";
+    }
+}
